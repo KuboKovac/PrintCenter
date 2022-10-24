13b95f06fb2879b3e279c21cb1f28bc88b9272ee
JakubKovac <jakub.kovac@kosickaakademia.sk>
2022-10-24 20:48:12 +0200

    FE - Added basic stylesheet for dark/light mode BE - Added entity framework for SQlite database

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {LoginComponent} from "../auth/login/login.component";
+import {RegisterComponent} from "../auth/register/register.component";
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    this.dialog.open(LoginComponent)
+  }
+
+  register(){
+    this.dialog.open(RegisterComponent)
   }
 
 }
