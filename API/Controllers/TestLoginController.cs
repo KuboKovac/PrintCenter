b6771f696d4268e9b0c6f177508dbf2cd22a0231
JakubKovac <jakub.kovac@kosickaakademia.sk>
2022-11-03 09:14:55 +0100

    Fix commit : FE: fixed responsibility issues on home page

---
+++ API/Controllers/TestLoginController.cs
@@ -0,0 +1,12 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class TestLoginController : ControllerBase
+{
+    
+}
---
+++ API/Models/TestLogin.cs
@@ -0,0 +1,7 @@
+﻿namespace API.Models;
+
+public class TestLogin
+{
+    public string name { get; set; }
+    public string email { get; set; }
+}
