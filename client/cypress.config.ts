5b5b3e1b42a16432b5a34ab48eea955050c5165e
Dekľo <106304843+Dekelino@users.noreply.github.com>
2023-01-08 00:48:36 +0100

    Update cypress.config.ts

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   component: {
     devServer: {
-      framework: "angular",
-      bundler: "webpack",
+      framework: 'angular',
+      bundler: 'webpack',
     },
-    specPattern: "**/*.cy.ts",
+    specPattern: '**/*.cy.ts',
   },
 
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:4200',
+    projectId: 'eyeds4',
   },
 });
