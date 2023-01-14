20312c059c4205bc40e8045ae746dc27a19fdeea
Dekľo <106304843+Dekelino@users.noreply.github.com>
2023-01-15 00:20:46 +0100

    added cypress + electron

--- client/cypress/e2e/darkMode.cy.ts
+++ client/cypress/e2e/darkMode.cy.ts
@@ -1,7 +1,6 @@
 describe('darkMode.cy.ts', () => {
     it('turn on darmode', () => {
       cy.visit('http://localhost:4200/home');
-      cy.wait(3000);
       cy.get('mat-slide-toggle').click();
     })
   })
--- client/cypress/e2e/loginRegistratedUser.cy.ts
+++ client/cypress/e2e/loginRegistratedUser.cy.ts
@@ -2,14 +2,12 @@ describe('wrongLogin.cy.ts', () => {
   it('After registration login registrated user', () => {
     cy.visit('http://localhost:4200/home');
     cy.get('button[id="cyLogin"]').click()
-
-
     cy.get('input[id="cyUsername"]').type('TestUsername')
     cy.get('input[id="cyPassword"]').type('jakopako')
-
     cy.get('button').contains('Login').click()
-
     cy.get('.mat-simple-snackbar').should('contain', 'Logged in successfully')
+    cy.get('button[id="cyLogout"]').click()
+
 
     
 
