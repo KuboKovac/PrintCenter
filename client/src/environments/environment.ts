276fa3b51d81ec4a01e11e27a97b9d29dc87f855
Dekľo <106304843+Dekelino@users.noreply.github.com>
2023-01-22 23:36:31 +0100

    Update environment.ts

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://localhost:7258/api/'
+  baseUrl: 'https://localhost:5077/api/'
 };
 /*
  * For easier debugging in development mode, you can import the following file
