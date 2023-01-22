2f53d3d376d929fef89a8605081aca509e66cf3a
Dekľo <106304843+Dekelino@users.noreply.github.com>
2023-01-22 21:47:20 +0100

    Update darkMode.cy.ts

--- client/cypress/e2e/darkMode.cy.ts
+++ client/cypress/e2e/darkMode.cy.ts
@@ -1,6 +1,6 @@
 describe('darkMode.cy.ts', () => {
   it('turn on darkmode', () => {
-    cy.visit('http://localhost:4200/client/home');
+    cy.visit('http://localhost:4200/home');
     cy.get('mat-slide-toggle').click();
     cy.get('mat-slide-toggle').click();
     cy.get('h1').should('have.css', 'color', 'rgb(255, 255, 255)');
